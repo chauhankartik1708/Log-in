6b07a5b96850faf374b630543e3bab6f920dcb13
Kartik Singh Chauhan <lkchauhan.725@gmail.com>
2018-02-21 12:14:56 +0530

    made some changes

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let buttn = document.getElementById('btn');
+buttn.onclick = function () {
+    alert("You are logged in !!")
+}
+
+let los = document.getElementById('lost');
+los.onclick = function(){
+    alert("INVALID.")
+}
